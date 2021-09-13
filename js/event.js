1b4d928cc162ac7f9719e1587ae9d00d2e66fac5
ppotatoG <thgus2270@gmail.com>
2021-09-13 19:49:32 +0900

    스크롤에 따른 값 세팅

--- js/event.js
+++ js/event.js
@@ -10,7 +10,14 @@
             heightNum : 5,
             scrollHeight : 0,
             objs : {
-                container : document.querySelector('.section-text')
+                container : document.querySelector('.section-text'),
+                sticky1 : document.querySelector('.section-text__stiky:nth-child(2)'),
+                sticky2 : document.querySelector('.section-text__stiky:nth-child(3)'),
+                sticky3 : document.querySelector('.section-text__stiky:nth-child(4)'),
+                sticky4 : document.querySelector('.section-text__stiky:nth-child(5)'),
+            },
+            values : {
+                opacity : [0, 1]
             }
         },
         {
@@ -59,10 +66,39 @@
         document.body.setAttribute('id', `show-scene-${currentScene + 1}`);
     }
 
+    function calcValues(values, currentYOffset){
+        let rv;
+        let scrollRatio = currentYOffset / sceneInfo[currentScene].scrollHeight;
+
+        rv = scrollRatio * (values[1] - values[0]) + values[0]; 
+
+        return rv;
+    }
+
+    function playAnimation(){
+        const objs = sceneInfo[currentScene].objs;
+        const values = sceneInfo[currentScene].values;
+        const currentYOffset = yOffset - prevScrollHeight;
+
+        switch (currentScene) {
+            case 0:
+                let sticky1_in = calcValues(values.opacity, currentYOffset);
+                objs.sticky1.style.opacity = sticky1_in; 
+                break;
+            case 1:
+                // console.log('1');
+                break;
+            case 2:
+                // console.log('2');
+                break;
+            case 3:
+                // console.log('3');
+                break;
+        }
+    }
 
     function scrollLoop() {
         prevScrollHeight = 0;
-        console.log(currentScene);
 
         for(let i = 0; i < currentScene; i++){
             prevScrollHeight += sceneInfo[i].scrollHeight;
@@ -76,12 +112,14 @@
             currentScene --;
             document.body.setAttribute('id', `show-scene-${currentScene + 1}`);
         }
+
+        playAnimation();
     }
 
     window.addEventListener('scroll', () => {
         yOffset = window.pageYOffset;
 
-        scrollLoop()
+        scrollLoop();
     });
 
     window.addEventListener('load', setLayout);
